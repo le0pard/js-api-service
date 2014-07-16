1ff2759abe47de1a41cbfc7b1b87f0a5e8e12a93
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-07-16 14:11:11 +0300

    fix routes

--- lib/js_api_service/controllers/js_function.ex
+++ lib/js_api_service/controllers/js_function.ex
@@ -2,15 +2,13 @@ defmodule JsApiService.Controllers.JsFunction do
   use Phoenix.Controller
 
   def run(conn, %{"function" => js_function, "data" => js_data}) when is_binary(js_function) and is_binary(js_data) do
-    response = case JsApiService.ErlangV8.eval_function({:global, :main}, js_function, [js_data]) do
-      {status, data} -> [{:status, status}, {:result, data}]
-    end
-    {:ok, json_response} = JSEX.encode response
+    { status, data } = JsApiService.ErlangV8.eval_function({:global, :main}, js_function, [js_data])
+    { :ok, json_response } = JSEX.encode([{ :status, status }, { :result, data }])
     json conn, json_response
   end
 
   def run(conn, _params) do
-    {:ok, json_response} = JSEX.encode([{:status, :error}, {:result, "You must provide `function` and `data` in POST params"}])
+    { :ok, json_response } = JSEX.encode([{ :status, :error }, { :result, "You must provide `function` and `data` in POST params" }])
     json conn, json_response
   end
 end
